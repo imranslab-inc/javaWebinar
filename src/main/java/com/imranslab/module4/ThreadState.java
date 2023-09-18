a5fe863d85fab642e3826becb6b25dc7c28beb3f
imranul-i <imranul.islam@imranslab.com>
2023-09-17 20:43:42 -0400

    File Reformatted.

--- src/main/java/com/imranslab/module4/ThreadState.java
+++ src/main/java/com/imranslab/module4/ThreadState.java
@@ -1,16 +1,34 @@
 package com.imranslab.module4;
 
 public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        // New State
+        Thread myThread = new Thread(new MyRunnable(), "t1");
+        System.out.println("Thread t1 state :" + myThread.getState());
 
-    static class MyRunnable implements Runnable
-    {
+        // Runnable state
+        myThread.start();
+        System.out.println("Thread t1 state :" + myThread.getState());
 
+        // Timed waiting state, thread will sleep
+        Thread.sleep(500);
+        System.out.println("Thread t1 state :" + myThread.getState());
+
+        // These two Println functions are not showing the right states. Bug!
+        // Waiting State
+        myThread.join();
+        System.out.println("Thread t1 state :" + myThread.getState());
+
+        // Terminated state
+        Thread.sleep(1000);
+        System.out.println("Thread t1 state :" + myThread.getState());
+    }
+
+    static class MyRunnable implements Runnable {
         @Override
         public void run() {
-            for (int i=0; i<30; i++)
-            {
-                try
-                {
+            for (int i = 0; i < 30; i++) {
+                try {
                     System.out.println(Thread.currentThread().getName() + " Count: " + i);
                     Thread.sleep(100);
 
@@ -18,30 +36,6 @@ public class ThreadState {
                     e.printStackTrace();
                 }
             }
-
         }
-    }
-    public static void main(String[] args) throws InterruptedException
-    {
-        // New State
-        Thread myThread = new Thread(new MyRunnable(), "t1");
-        System.out.println("Thread t1 state :" +myThread.getState());
-
-        // Runnable state
-        myThread.start();
-        System.out.println("Thread t1 state :" +myThread.getState());
-
-        // Timed waiting state, thread will sleep
-        Thread.sleep(500);
-        System.out.println("Thread t1 state :" +myThread.getState());
-
-        // These two Println functions are not showing the right states. Bug!
-        // Waiting State
-        myThread.join();
-        System.out.println("Thread t1 state :" +myThread.getState());
-
-        // Terminated state
-        Thread.sleep(1000);
-        System.out.println("Thread t1 state :" +myThread.getState());
     }
 }
